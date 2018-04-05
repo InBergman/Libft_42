ff25ad1d0582722016a646f0cf2b5c0b6d42213e
InBergman <mberedda42@gmail.com>
2018-04-05 11:36:51 +0200

    All Function

---
+++ ft_itoa.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mberedda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/04 17:10:47 by mberedda          #+#    #+#             */
+/*   Updated: 2015/12/07 20:58:47 by mberedda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+static int		ft_intlength(int n)
+{
+	int			i;
+
+	i = 1;
+	if (n < 0)
+	{
+		i++;
+		n = -n;
+	}
+	while (n > 9)
+	{
+		i++;
+		n = n / 10;
+	}
+	return (i);
+}
+
+static char		*ft_intmax(void)
+{
+	char		*str;
+
+	str = ft_strnew(11);
+	if (!str)
+		return (NULL);
+	ft_strcpy(str, "-2147483648");
+	return (str);
+}
+
+char			*ft_itoa(int n)
+{
+	char		*str;
+	int			length;
+	int			is_negative;
+
+	length = ft_intlength(n);
+	if (n == -2147483648)
+		return (ft_intmax());
+	str = ft_strnew(length + 1);
+	if (!str)
+		return (NULL);
+	str[length] = '\0';
+	if (n < 0)
+	{
+		is_negative = 1;
+		n = -n;
+	}
+	while (n > 9)
+	{
+		str[--length] = (n % 10) + 48;
+		n = n / 10;
+	}
+	str[--length] = (n % 10) + 48;
+	if (is_negative)
+		str[--length] = '-';
+	return (str);
+}
